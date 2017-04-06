290fcec12aa2c0bd02e0b3e243e2dfa816fd8d58
Matt Robinson <mattrobinson125@gmail.com>
2017-04-06 18:15:12 -0400

    added listener for a test socket message. Added client side example message on button click.

--- index.js
+++ index.js
@@ -8,6 +8,17 @@ app.get('/', function(req, res){
 
 io.on('connection', function(socket){
   console.log('a user connected');
+
+  // User sends an example message
+  socket.on('example message', function(msg){
+    console.log(msg);
+  });
+
+  // User Disconnected
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+
 });
 
 http.listen(process.env.PORT || 3000, function(){
