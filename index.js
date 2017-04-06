a1976bff4274071bbc1d550a360f9e1bfe02b3d8
Matt Robinson <mattrobinson125@gmail.com>
2017-04-06 18:16:37 -0400

    added call to imported files function

--- index.js
+++ index.js
@@ -2,12 +2,18 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+// Import data management modules
+var shift = require('./shiftmanager');
+
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
   console.log('a user connected');
+
+  // call method from module
+  shift.test();
 
   // User sends an example message
   socket.on('example message', function(msg){
